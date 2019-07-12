1656bc21166364fb4aab16a7dfe8ef8eeeabf245
david-stan <davidstanojevic43@gmail.com>
2019-07-12 19:14:42 +0200

    Add FluentValidator and validate input models

---
+++ TimeTrackerApp/Models/Validation/ClientInputModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTrackerApp.Models.Validation
+{
+    public class ClientInputModelValidator
+        : AbstractValidator<ClientInputModel>
+    {
+        public ClientInputModelValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .Length(1, 100);
+        }
+    }
+}
---
+++ TimeTrackerApp/Models/Validation/TimeEntryInputModelValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTrackerApp.Models.Validation
+{
+    public class TimeEntryInputModelValidator
+        : AbstractValidator<TimeEntryInputModel>
+    {
+        public TimeEntryInputModelValidator()
+        {
+            RuleFor(x => x.UserId)
+                .GreaterThan(0);
+
+            RuleFor(x => x.ProjectId)
+                .GreaterThan(0);
+
+            RuleFor(x => x.EntryDate)
+                .GreaterThan(new DateTime(2019, 1, 1))
+                .LessThan(new DateTime(2100, 1, 1));
+
+            RuleFor(x => x.Hours)
+                .ExclusiveBetween(0, 25);
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .MaximumLength(10000);
+        }
+    }
+}
