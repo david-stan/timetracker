030df86a98f66cc53eec3b2c8e2153dedbe806a0
david-stan <davidstanojevic43@gmail.com>
2019-07-12 16:34:30 +0200

    Add models, input models and paging.

---
+++ TimeTrackerApp/Models/PagedList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTrackerApp.Models
+{
+    public class PagedList<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int Page { get; set; } //trenutna stranica
+        public int PageSize { get; set; } //velicina stranice
+        public int TotalCount { get; set; } //ukupan broj entiteta
+        public int TotalPages => 
+            (int) Math.Ceiling(TotalCount / (decimal)PageSize);
+    }
+}
---
+++ TimeTrackerApp/Models/ProjectInputModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTrackerApp.Models
+{
+    public class ProjectInputModel
+    {
+        public string Name { get; set; }
+        public long ClientId { get; set; }
+    }
+}
