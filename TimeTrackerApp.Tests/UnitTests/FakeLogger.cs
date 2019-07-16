29a3eb9b0c5e716b0331c753f931c82ab616d3a1
david-stan <davidstanojevic43@gmail.com>
2019-07-16 17:35:29 +0200

    Add unit tests

---
+++ TimeTrackerApp.Tests/UnitTests/FakeLogger.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeTrackerApp.Tests.UnitTests
+{
+    public class FakeLogger<T> : ILogger<T>, IDisposable
+    {
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return false;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+        }
+    }
+}
